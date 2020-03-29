ce5d0f49fc99069aeb1b6a9cc57da867540adbb6
iamjuvy <iamjuvy@gmail.com>
2020-03-29 18:40:41 -0500

    angular project

---
+++ my-app/src/app/products.ts
@@ -0,0 +1,17 @@
+export const products = [
+    {
+      name: 'Vegetable Samosa',
+      price: 4,
+      description: 'Deep fried crisp flour patties filled with a delicious blend of spiced potatoes & peas'
+    },
+    {
+      name: 'Onion Bajji',
+      price: 5,
+      description: 'Chopped onions coated in spiced chickpea flour batter and deep fried'
+    },
+    {
+      name: 'Phone Standard',
+      price: 299,
+      description: ''
+    }
+  ];
